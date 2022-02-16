76684c7e7fddc9cc8a397fabfe5a5d1e4bb50d42
zinedburak <zined_burak@hotmail.com>
2022-02-16 14:47:17 +0300

    Creat controller functions for different functionalities of the assigment

---
+++ controllers/controllers.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"Key_Value_Storage/models"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
+var Db *models.Store
+
+func GetKeyValue(c *fiber.Ctx) error {
+
+	var cKey map[string]string
+	if err := c.BodyParser(&cKey); err != nil {
+		fmt.Println(err)
+	}
+	return c.JSON(Db.Get(cKey["key"]))
+}
+
+func SetKeyValue(c *fiber.Ctx) error {
+	var cKeyValue models.KeyValue
+
+	if err := c.BodyParser(&cKeyValue); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	Db.Set(cKeyValue.Key, cKeyValue.Value)
+	return c.JSON(cKeyValue.Key)
+}
+
+func GetAllKeyValue(c *fiber.Ctx) error {
+	return c.JSON(Db.GetAll())
+}
+
+func FlushAllData(c *fiber.Ctx) error {
+	Db.FlushAllData()
+	return c.JSON(Db.GetAll())
+}
