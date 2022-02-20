0efc10c3e89559603b4704427b2eee5feea74263
zinedburak <zined_burak@hotmail.com>
2022-02-20 17:57:33 +0300

    Controllers change to eliminate the dependencies to non-standard libraries

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,37 +2,108 @@ package controllers
 
 import (
 	"Key_Value_Storage/models"
+	"encoding/json"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
+	"io/ioutil"
+	"net/http"
 )
 
 var Db *models.Store
 
-func GetKeyValue(c *fiber.Ctx) error {
-
-	var cKey map[string]string
-	if err := c.BodyParser(&cKey); err != nil {
-		fmt.Println(err)
+func GetKeyValue(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		ct := r.Header.Get("content-type")
+		if ct != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte(fmt.Sprintf("Content type must be 'application/json, but you provide '%s'", ct)))
+		}
+		var cKey map[string]string
+		if err := json.Unmarshal(bodyBytes, &cKey); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		jsonBytes, err := json.Marshal(Db.Get(cKey["key"]))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
 	}
-	return c.JSON(Db.Get(cKey["key"]))
 }
 
-func SetKeyValue(c *fiber.Ctx) error {
-	var cKeyValue models.KeyValue
+func SetKeyValue(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		ct := r.Header.Get("content-type")
+		if ct != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte(fmt.Sprintf("Content type must be 'application/json, but you provide '%s''", ct)))
+		}
+		var cKeyValue models.KeyValue
+		if err := json.Unmarshal(bodyBytes, &cKeyValue); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		Db.Set(cKeyValue.Key, cKeyValue.Value)
+		return
 
-	if err := c.BodyParser(&cKeyValue); err != nil {
-		fmt.Println(err)
-		return err
 	}
-	Db.Set(cKeyValue.Key, cKeyValue.Value)
-	return c.JSON(cKeyValue)
+
 }
 
-func GetAllKeyValue(c *fiber.Ctx) error {
-	return c.JSON(Db.GetAll())
+func GetAllKeyValue(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		jsonBytes, err := json.Marshal(Db.GetAll())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+	}
 }
 
-func FlushAllData(c *fiber.Ctx) error {
-	Db.FlushAllData()
-	return c.JSON(Db.GetAll())
+func FlushAllData(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		Db.FlushAllData()
+		jsonBytes, err := json.Marshal(Db.GetAll())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+	}
 }
