7634acc0550ad8249f0cb0aa5ef94fa80c62d296
zinedburak <zined_burak@hotmail.com>
2022-02-20 17:59:13 +0300

    Main change to eliminate the dependencies to non-standard libraries

--- main.go
+++ main.go
@@ -4,21 +4,18 @@ import (
 	"Key_Value_Storage/controllers"
 	"Key_Value_Storage/models"
 	"Key_Value_Storage/routes"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+	"net/http"
 	"time"
 )
 
 var Db models.Store
 
 func main() {
-	Db := models.CreateStore("HelloWorld.json", 5*time.Second)
+	Db := models.CreateStore("HelloWorld.json", 5*time.Hour)
 	controllers.Db = Db
-
-	app := fiber.New()
-	app.Use(cors.New(cors.Config{AllowCredentials: true}))
-
-	routes.Setup(app)
-
-	app.Listen(":8000")
+	routes.Setup()
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
